e21889228e324626c4c51ef473f1660d73cee26a
Angela Carrillo35 <aa.carrillom@uea.edu.ec>
2025-10-05 22:49:19 -0500

    Tarea de Lectura de archivo con comentarios

--- PythonProject-temperatura/lecturaArchivo.py
+++ PythonProject-temperatura/lecturaArchivo.py
@@ -1,16 +1,16 @@
-def leer_archivo_linea_por_linea(nombre_archivo):
-    try:
-        with open(nombre_archivo, 'r',) as archivo:
-            print(f"Contenido de '{nombre_archivo}' de Angela Carrillo:")
-            for linea in archivo:
-                print(linea.strip())
-    except FileNotFoundError:
-        print(f"Error: El archivo '{nombre_archivo}' no existe.")
-    except IOError:
-        print(f"Error: No se pudo abrir el archivo '{nombre_archivo}'.")
+def leer_archivo_linea_por_linea(nombre_archivo): #Definicion de la función leer archivos
+    try: #Control de excepciones
+        with open(nombre_archivo, 'r',) as archivo: #apertura de archivo enviado en la función
+            print(f"Contenido de '{nombre_archivo}' de Angela Carrillo:") #Imprime una etiqueta con mi nombre
+            for linea in archivo: #repite la lectura linea por linea hasta completar el archivo
+                print(linea.strip()) #impresión de la linea omitiendo salto de página
+    except FileNotFoundError: #captura del error al no existir el archivo
+        print(f"Error: El archivo '{nombre_archivo}' no existe.") #Impresión de leyenda del error
+    except IOError: #Captura del error al no poder abrirlo
+        print(f"Error: No se pudo abrir el archivo '{nombre_archivo}'.") #Impresión de leyenda del error
 
-# Ruta completa del archivo
-ruta_archivo = r'my_notes.txt'
+# Definición de la ruta completa del archivo
+ruta_archivo = r'my_notes.txt' 
 
-# Llamada a la función
+# Llamada a la función lectura de archivos
 leer_archivo_linea_por_linea(ruta_archivo)
